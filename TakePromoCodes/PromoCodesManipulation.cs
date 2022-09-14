07bfacd48c0409d1e7d12c35736267598a89d7ae
SofiiaFlys <flyssoffia@gmail.com>
2022-09-14 21:36:31 +0300

    TakePromoCodes project modifications

---
+++ TakePromoCodes/PromoCodesManipulation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TakePromoCodes
+{
+    public class PromoCodesManipulation
+    {
+        private PromoCodes m_requiredPromoCodes;
+        public PromoCodes RequiredPromoCodes
+        {
+            get { return m_requiredPromoCodes; }
+            set { m_requiredPromoCodes = value; }
+        }
+
+        public PromoCodesManipulation()
+        {
+            RequiredPromoCodes.Codes = new List<String>();
+        }
+
+        public PromoCodesManipulation(PromoCodes requiredPromoCodes)
+        {
+            m_requiredPromoCodes = requiredPromoCodes;
+        }
+
+        public void ChangePromoCodesFormat_SZGR()
+        {
+            List<string> replacedCodesInRightFormat = new List<string>();
+            foreach (var code in RequiredPromoCodes.Codes)
+            {
+                if (!code.Equals(String.Empty))
+                {
+                    var delimitedValues = code.Split(',');
+                    var promoCode = delimitedValues[1].Replace('"', ' ').Trim();
+                    replacedCodesInRightFormat.Add(promoCode);
+                }
+            }
+            RequiredPromoCodes.Codes = replacedCodesInRightFormat;
+        }
+
+    }
+}
