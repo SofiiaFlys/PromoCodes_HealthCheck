dc00002fa2117067f207e595f1c0ee68d6feb43f
Sofiia Flys <sflys@softserveinc.com>
2025-03-14 14:32:55 +0200

    Add Abstract class

---
+++ TakePromoCodes/BaseCodes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TakePromoCodes
+{
+    public abstract class BaseCodes
+    {
+        public abstract List<String> Codes { get; set; }
+    }
+}
