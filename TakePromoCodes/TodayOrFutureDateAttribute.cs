2a4a663b2094f7552bcda98186a55edbd7b96177
Sofiia Flys <sflys@softserveinc.com>
2025-03-16 23:29:11 +0200

    Add Custom TodayOrFutureDateAttribute

---
+++ TakePromoCodes/TodayOrFutureDateAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TakePromoCodes
+{
+    public class TodayOrFutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateValue)
+            {
+                if (dateValue >= DateTime.Now.Date)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Expiry date must be today or a future date.");
+                }
+            }
+
+            return new ValidationResult("Invalid expiry date format.");
+        }
+    }
+}
